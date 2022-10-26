7c2d34c5322d21959832da07996fe653eef96722
ruofusun <42979525+ruofusun@users.noreply.github.com>
2022-10-26 02:15:08 -0400

    Buggy cut scene

--- Assets/Van/Script/BodyAniEvent.cs
+++ Assets/Van/Script/BodyAniEvent.cs
@@ -7,11 +7,26 @@ public class BodyAniEvent : MonoBehaviour
     public GameObject player;
     public MouseLook mouseLook;
     public FakePlayer fakePl;
+    public Animator animator;
+    
 
     public void GiveLookRight()
     {
         mouseLook.enabled = true;
         mouseLook.isLookUp = true;
         fakePl.isLookUp = true;
+        if (animator)
+        {
+            animator.enabled = true;
+            GetComponent<Animator>().enabled = false;
+            fakePl.transform.eulerAngles = new Vector3(player.transform.rotation.eulerAngles.x,
+                player.transform.rotation.eulerAngles.y, player.transform.rotation.eulerAngles.z);
+
+            //  Vector3 pos = animator.gameObject.transform.position+ animator.gameObject.transform.parent.position;
+            //  animator.gameObject.transform.SetParent(null);
+            //   animator.gameObject.transform.position = pos;
+        }
     }
+    
+    
 }
